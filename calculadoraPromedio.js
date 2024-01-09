00c0462ffc90cb402dc69c1b78cbc8b189c67e6d
Claudia <claudiaivettlopez@yahoo.com>
2024-01-09 02:08:58 -0500

    first commit

---
+++ calculadoraPromedio.js
@@ -0,0 +1,33 @@
+let nota1= parseInt(prompt("Ingresar nota1"))
+let nota2= parseInt(prompt("Ingresar nota2"))
+let nota3= parseInt(prompt("Ingresar nota3"))
+ 
+let promedio=(nota1+nota2+nota3)/3
+parseInt(promedio)
+
+console.log=("Nombre Alumno");
+console.log=("Nombre Materia");
+
+if(nota1>=0 && nota1<=10){
+    alert=("Se evalua la nota");
+} else(nota1<=0 && nota1>=10); {
+    alert=("Error en el dato");
+}
+
+if(nota2>=0 && nota2<=10){
+    alert=("Se evalua la nota");
+} else if(nota2<=0 && nota2>=10); {
+    alert=("Error en el dato");
+}
+
+if(nota3>=0 && nota3<=10){
+    alert=("Se evalua la nota");
+} else(nota3<=0 && nota3>=10); {
+    alert=("Error en el dato");
+}
+
+if(promedio>=7){
+    console.log=("Nombre alumno Felicidades has aprobado!!");
+} else if(promedio<7); {
+    alert=("Lo sentimos, no has aprobado");
+}
